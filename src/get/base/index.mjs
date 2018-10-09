8ed3d9a3f1add9e38e3f47d16cb3047027e7b62b
Dino <dinoabsoluto+dev@gmail.com>
2018-10-09 17:10:16 +0700

    Move to subfolder

---
+++ src/get/base/index.mjs
@@ -0,0 +1,25 @@
+/**
+ * @file Get module
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * This file is part of translator-js.
+ *
+ * translator-js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * translator-js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with translator-js.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/* imports */
+/* -imports */
+/* exports */
+export { default as Chapter } from './chapter'
+export { default as Volume } from './volume'
