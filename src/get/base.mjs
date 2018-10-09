802d575178eb482f94b5ba50e0f6f4fa8dcdcb73
Dino <dinoabsoluto+dev@gmail.com>
2018-10-09 10:50:02 +0700

    Add base.mjs & volume.mjs

---
+++ src/get/base.mjs
@@ -0,0 +1,45 @@
+/**
+ * @file Get module
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * This file is part of translator-js.
+ *
+ * translator-js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * translator-js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with translator-js.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/* imports */
+/* -imports */
+export default class Base {
+  constructor (props = {}) {
+    if (typeof props !== 'object') {
+      throw new Error('props has to be an object')
+    }
+    Object.defineProperties(this, {
+      props: { value: props }
+    })
+  }
+
+  updateProps (props) {
+    if (typeof props !== 'object') {
+      return
+    }
+    Object.assign(this.props, props)
+  }
+}
+
+export class File extends Base {
+  get isDir () {
+    return this.props.isDir
+  }
+}
