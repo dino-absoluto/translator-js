4cda12797b5f3e011059f880687a4b44bda385f4
Dino <dinoabsoluto+dev@gmail.com>
2019-04-08 14:12:12 +0700

    Unify License block

--- src/get/providers/__tests__/syosetu.test.ts
+++ src/get/providers/__tests__/syosetu.test.ts
@@ -1,21 +1,21 @@
 /**
- * @file Test Syosetu
  * @author Dino <dinoabsoluto+dev@gmail.com>
  * @license
- * This file is part of translator-js.
+ * Translator-js - Scripts to facilitate Japanese webnovel
+ * Copyright (C) 2019 Dino <dinoabsoluto+dev@gmail.com>
  *
- * translator-js is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * translator-js is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with translator-js.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 /* imports */
