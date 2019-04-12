9adb7f6720b6848f509a699a0ee97bc7952694de
Dino <dinoabsoluto+dev@gmail.com>
2019-04-12 22:29:17 +0700

    Update progress bar

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -22,7 +22,6 @@
 import { parser as yargs } from './shared'
 import * as cmdGet from '../get/cli-interface'
 import chalk from 'chalk'
-import { Progress } from '../utils/progress'
 
 try {
   yargs
@@ -36,79 +35,6 @@ try {
         console.error(chalk.red(msg))
       }
     })
-    .command('test', false,
-      yargs =>
-        yargs,
-      async _argv => {
-        const progress = new Progress.Progress()
-        const group = new Progress.Group({ flexShrink: 0 })
-        const barLeft = new Progress.Bar({
-          width: 20,
-          postProcess: (...cols) => {
-            return chalk.green(cols[0]) + chalk.yellow(cols[1]) + chalk.gray(cols[2])
-          }
-        })
-        group.add(new Progress.Text({ text: '⸨', flexShrink: 0 }))
-        group.add(barLeft)
-        group.add(new Progress.Text({ text: '⸩', flexShrink: 0 }))
-        group.add(new Progress.Space())
-        group.add(new Progress.Text({
-          text: 'Hello! Welcome to Hell!',
-          align: Progress.TextAlignment.Left,
-          postProcess: chalk.green
-        }))
-        progress.add(group)
-        const spinner = new Progress.Spinner({ postProcess: chalk.blue })
-        group.add(spinner)
-        progress.add(spinner)
-        progress.add(new Progress.Spinner({ postProcess: chalk.yellow }))
-        progress.add(new Progress.Spinner({ postProcess: chalk.yellow }))
-        progress.add(new Progress.Space({ width: 5 }))
-        const label = new Progress.Text({
-          text: 'Hello World!',
-          flex: 1,
-          postProcess: chalk.gray
-        })
-        progress.add(label)
-        progress.add(new Progress.Space())
-        const bar = new Progress.Bar({
-          width: 20,
-          flex: 1,
-          ratio: .1,
-          postProcess: (...cols) => {
-            return chalk.gray(cols[2]) + chalk.yellow(cols[1]) + chalk.green(cols[0])
-          }
-        })
-        progress.add(new Progress.Text({ text: '⸨', flexShrink: 0 }), 0)
-        progress.add(bar, 1)
-        progress.add(new Progress.Text({ text: '⸩', flexShrink: 0 }), 2)
-        progress.update()
-        let count = 0
-        let lcount = 0
-        const loop = setInterval(() => {
-          lcount++
-          label.text = `Hello World! ${lcount}`
-          barLeft.ratio += 0.03
-          bar.ratio += .015
-          if (barLeft.ratio >= 1) {
-            barLeft.ratio = 0
-          }
-          if (bar.ratio >= 1) {
-            if (label.parent) {
-              progress.delete(label)
-            }
-            progress.clearLine()
-            console.log('Looped')
-            bar.ratio = 0
-            if (count++ > 3) {
-              clearInterval(loop)
-              progress.clear()
-              console.log(progress.count * 1000 / progress.elapsed)
-            }
-          }
-        }, 40)
-        return
-      })
     .parse()
 } catch (err) {
   console.error(chalk.red((err.stack || '').toString()))
