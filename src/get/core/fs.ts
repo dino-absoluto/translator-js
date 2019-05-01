576564135be9d752f41ccecb9f1926d833c12d36
Dino <dinoabsoluto+dev@gmail.com>
2019-05-01 21:18:09 +0700

    Fix: linting errors in fs.ts

--- src/get/core/fs.ts
+++ src/get/core/fs.ts
@@ -23,10 +23,10 @@ import * as path from 'path'
 import makeDir = require('make-dir')
 /* code */
 
-const sanitizeName = (name?: string) => {
+const sanitizeName = (name?: string): string | undefined => {
   return flow(trim(name))
     .then(path.normalize)
-    .then(name => {
+    .then((name): string | undefined => {
       if (path.basename(name) !== name) {
         return
       }
@@ -46,14 +46,95 @@ interface FSItem {
   rename (name: string): Promise<void>
 }
 
+export class File implements FSItem {
+  public readonly parent: Folder
+  private _filename?: string
+
+  public constructor (parent: Folder, name: string) {
+    this.parent = parent
+    this._filename = sanitizeName(name)
+  }
+
+  public get path (): string {
+    const { parent, _filename } = this
+    if (!_filename) {
+      throw new Error('filename is not set')
+    }
+    return path.join(parent.path, _filename)
+  }
+
+  public async real (): Promise<void> {
+    if (!this._filename) {
+      throw new Error('filename is not set')
+    }
+  }
+
+  public async rename (name: string): Promise<void> {
+    const newName = sanitizeName(name)
+    if (!newName) {
+      throw new Error('filename is invalid')
+    }
+    const oldName = this._filename
+    this._filename = newName
+    if (oldName) {
+      if (newName === oldName) {
+        return
+      }
+      try {
+        const parentDir = this.parent.path
+        await pfs.rename(
+          path.join(parentDir, oldName),
+          path.join(parentDir, newName))
+      } catch (err) {
+        if (err.code !== 'ENOENT') {
+          throw err
+        }
+      }
+    }
+  }
+
+  public async close (): Promise<void> {
+    delete this._filename
+    this.parent.children.delete(this)
+  }
+
+  public async access (mode?: number): Promise<void> {
+    return pfs.access(this.path, mode)
+  }
+
+  public async remove (): Promise<void> {
+    await this.real()
+    const fpath = this.path
+    await this.close()
+    try {
+      await pfs.unlink(fpath)
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err
+      }
+    }
+  }
+
+  public async read (): Promise<Buffer> {
+    await this.real()
+    return pfs.readFile(this.path)
+  }
+
+  public async write (content: string | Buffer): Promise<void> {
+    await this.parent.real()
+    await this.real()
+    await pfs.writeFile(this.path, content)
+  }
+}
+
 export class Folder implements FSItem {
-  readonly children: Set<Folder | File> = new Set()
-  readonly parent?: Folder
+  public readonly children: Set<Folder | File> = new Set()
+  public readonly parent?: Folder
   /* private data */
   private _dirname?: string
   private _accessed = false
 
-  constructor (parent: Folder | undefined | null, name: string) {
+  public constructor (parent: Folder | undefined | null, name: string) {
     if (parent) {
       this.parent = parent
       this._dirname = sanitizeName(name)
@@ -62,8 +143,8 @@ export class Folder implements FSItem {
     }
   }
 
-  get name () { return this._dirname }
-  get path (): string {
+  public get name (): string | undefined { return this._dirname }
+  public get path (): string {
     const { parent, _dirname } = this
     if (!_dirname) {
       throw new Error('Folder\'s path is undefined')
@@ -74,23 +155,22 @@ export class Folder implements FSItem {
     return _dirname
   }
 
-  async real () {
+  public async real (): Promise<void> {
     if (this._accessed) {
-
     } else {
       await makeDir(this.path)
       this._accessed = true
     }
   }
 
-  async close () {
+  public async close (): Promise<void> {
     delete this._dirname
     if (this.parent) {
       this.parent.children.delete(this)
     }
   }
 
-  async remove () {
+  public async remove (): Promise<void> {
     const { children } = this
     if (!children.size) {
       throw new Error('folder is not empty')
@@ -100,8 +180,8 @@ export class Folder implements FSItem {
     await pfs.rmdir(fpath)
   }
 
-  get renameable () { return !!this.parent }
-  async rename (name: string) {
+  public get renameable (): boolean { return !!this.parent }
+  public async rename (name: string): Promise<void> {
     if (!this.parent) {
       throw new Error('folder cannot be renamed')
     }
@@ -129,96 +209,15 @@ export class Folder implements FSItem {
     }
   }
 
-  requestFolder (name: string): Folder {
+  public requestFolder (name: string): Folder {
     const entry = new Folder(this, name)
     this.children.add(entry)
     return entry
   }
 
-  requestFile (name: string): File {
+  public requestFile (name: string): File {
     const entry = new File(this, name)
     this.children.add(entry)
     return entry
-  }
-}
-
-export class File implements FSItem {
-  readonly parent: Folder
-  private _filename?: string
-
-  constructor (parent: Folder, name: string) {
-    this.parent = parent
-    this._filename = sanitizeName(name)
-  }
-
-  get path (): string {
-    const { parent, _filename } = this
-    if (!_filename) {
-      throw new Error('filename is not set')
-    }
-    return path.join(parent.path, _filename)
-  }
-
-  async real () {
-    if (!this._filename) {
-      throw new Error('filename is not set')
-    }
-  }
-
-  async rename (name: string) {
-    const newName = sanitizeName(name)
-    if (!newName) {
-      throw new Error('filename is invalid')
-    }
-    const oldName = this._filename
-    this._filename = newName
-    if (oldName) {
-      if (newName === oldName) {
-        return
-      }
-      try {
-        const parentDir = this.parent.path
-        await pfs.rename(
-          path.join(parentDir, oldName),
-          path.join(parentDir, newName))
-      } catch (err) {
-        if (err.code !== 'ENOENT') {
-          throw err
-        }
-      }
-    }
-  }
-
-  async close () {
-    delete this._filename
-    this.parent.children.delete(this)
-  }
-
-  async access (mode?: number) {
-    return pfs.access(this.path, mode)
-  }
-
-  async remove () {
-    await this.real()
-    const fpath = this.path
-    await this.close()
-    try {
-      await pfs.unlink(fpath)
-    } catch (err) {
-      if (err.code !== 'ENOENT') {
-        throw err
-      }
-    }
-  }
-
-  async read () {
-    await this.real()
-    return pfs.readFile(this.path)
-  }
-
-  async write (content: string | Buffer) {
-    await this.parent.real()
-    await this.real()
-    await pfs.writeFile(this.path, content)
   }
 }
